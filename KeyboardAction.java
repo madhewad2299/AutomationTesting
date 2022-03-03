d857108a5e9fbedd95b61585586b6c87b5f9589e
madhewad2299 <100014762+madhewad2299@users.noreply.github.com>
2022-03-03 19:51:11 +0530

    Add files via upload

---
+++ KeyboardAction.java
@@ -0,0 +1,18 @@
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class KeyboardAction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "E:\\Testing\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+		driver.get("https://the-internet.herokuapp.com/key_presses");
+		Actions act=new Actions(driver);
+	//	act.sendKeys(Keys.ENTER).perform();
+		act.sendKeys(Keys.CANCEL).perform();
+	}
+
+}
